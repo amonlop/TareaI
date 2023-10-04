a88a22dfd454d1d41756e7144c9063456b6c7ebe
Diego-ipt <106196745+Diego-ipt@users.noreply.github.com>
2023-10-04 15:11:30 -0300

    Update OrdenCompra.java
    +methods

--- tarea1/src/main/java/com/example/OrdenCompra.java
+++ tarea1/src/main/java/com/example/OrdenCompra.java
@@ -4,16 +4,17 @@ import java.util.ArrayList;
 import java.util.Date;
 
 class OrdenCompra {
-    private ArrayList<OrdenCompra> OrdenCompra;
+    private ArrayList<DetalleOrden> DetalleOrden;
     private ArrayList<Pago> pagos;
     private DocTributario docTributario;
     private Cliente cliente;
     private Date fecha;
     private String estado;
 
-    public OrdenCompra(Date fecha, String estado) {
+    public OrdenCompra(Date fecha, String estado, ArrayList<DetalleOrden> DetalleOrden) {
         this.fecha = fecha;
         this.estado = estado;
+        this.DetalleOrden=DetalleOrden;
     }
 
     public Date getFecha() {
@@ -30,5 +31,33 @@ class OrdenCompra {
 
     public void setEstado(String estado) {
         this.estado = estado;
+    }
+        public float calcPrecioSinIVA(){
+        float aux=0;
+        for (int i = 0; i < DetalleOrden.size(); i++) {
+            aux=aux+this.DetalleOrden.get(i).calcPrecioSinIVA();
+        }
+        return aux;
+    }
+    public float calcIVA(){
+        float aux=0;
+        for (int i = 0; i < DetalleOrden.size(); i++) {
+            aux=aux+this.DetalleOrden.get(i).calcIVA();
+        }
+        return aux;
+    }
+    public float calcPrecio(){
+        float aux=0;
+        for (int i = 0; i < DetalleOrden.size(); i++) {
+            aux=aux+this.DetalleOrden.get(i).calcPrecio();
+        }
+        return aux;
+    }
+    public float calcPeso(){
+        float aux=0;
+        for (int i = 0; i < DetalleOrden.size(); i++) {
+            aux=aux+this.DetalleOrden.get(i).calcPeso();
+        }
+        return aux;
     }
 }
