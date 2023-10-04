cae4992caf3fbfd8f1865160d43bae470aa64e06
Antonia <montlopanto@gmail.com>
2023-10-04 13:09:39 -0300

    Clases articulo y DetalleOrden creadas con respectivos getters y setters

--- tarea1/src/main/java/com/example/Articulo.java
+++ tarea1/src/main/java/com/example/Articulo.java
@@ -1,1 +1,45 @@
+package com.example;
 
+class Articulo {
+    private float peso;
+    private String nombre;
+    private String descripcion;
+    private float precio;
+
+    public Articulo() {
+
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    
+}
--- tarea1/src/main/java/com/example/DetalleOrden.java
+++ tarea1/src/main/java/com/example/DetalleOrden.java
@@ -1,1 +1,35 @@
+package com.example;
 
+class DetalleOrden {
+    private int cantidad;
+
+    public DetalleOrden() {
+
+    }
+
+    //con IVA
+    public float calcPrecio() {
+        return 1f; //no es este resultado, es solo pa q no marque error
+    }
+
+    public float calcPrecioSinIVA() {
+        return 1f;
+    }
+
+    public float calcIVA() {
+        return 1f;
+    }
+
+    public float calcPeso() {
+        return 1f;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+    
+}
