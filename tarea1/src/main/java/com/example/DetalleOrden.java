6929d2ea6167535b456d6f0409a930da543e5de0
Diego-ipt <106196745+Diego-ipt@users.noreply.github.com>
2023-10-04 14:03:58 -0300

    Update DetalleOrden.java
    +cardinalidad
    +constructor
    +tostring

--- tarea1/src/main/java/com/example/DetalleOrden.java
+++ tarea1/src/main/java/com/example/DetalleOrden.java
@@ -1,13 +1,14 @@
 package com.example;
-
 class DetalleOrden {
     private int cantidad;
-
-    public DetalleOrden() {
-
+    private Articulo articulo;
+    public DetalleOrden(Articulo articulo, int cantidad) {
+        this.cantidad=cantidad;
+        this.articulo=articulo;
     }
-
-    //con IVA
+    public String tostring(){
+        return "articulo: "+this.articulo.getNombre() +", cantidad: "+this.cantidad;
+    }
     public float calcPrecio() {
         return 1f; //no es este resultado, es solo pa q no marque error
     }
@@ -31,5 +32,5 @@ class DetalleOrden {
     public int getCantidad() {
         return cantidad;
     }
-    
+
 }
