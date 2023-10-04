1acfc9510c219800d0a1e4c85bb3bbf812257130
Diego-ipt <106196745+Diego-ipt@users.noreply.github.com>
2023-10-04 13:08:24 -0300

    Update OrdenCompra.java
    agregaciones básicas

--- tarea1/src/main/java/com/example/OrdenCompra.java
+++ tarea1/src/main/java/com/example/OrdenCompra.java
@@ -1,1 +1,32 @@
+package com.example;
 
+public class DetalleOrden {
+    private Date fecha;
+    private String estado;
+
+    public DetalleOrden(Date fecha, String estado) {
+        this.fecha = fecha;
+        this.estado = estado;
+    }
+
+    public DetalleOrden(Date fecha, String estado) {
+        this.fecha = fecha;
+        this.estado = estado;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
